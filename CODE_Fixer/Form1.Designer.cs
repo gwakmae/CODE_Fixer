b15407f44380c95a58927481e30119c0bf856574
Sungjae Kwak <piagwak@gmail.com>
2025-09-03 15:48:16 +0900

    eat: Add MQL variants and always on top option
    - Add MQL4, MQH, and MQ5 language buttons to replace single MQL5 button
    - Implement always on top checkbox with persistent setting storage
    - Enhance clipboard functionality with Windows API for better reliability
    - Update settings structure to support multiple configuration options
    - Add retry mechanism and fallback for clipboard operations
    
    - MQL5 버튼을 MQL4, MQH, MQ5 개별 버튼으로 분리하여 언어 지원 확장
    - 설정 저장 기능이 포함된 항상 위에 표시 체크박스 구현
    - Windows API를 활용한 향상된 클립보드 기능으로 안정성 개선
    - 다중 설정 옵션 지원을 위한 설정 구조 업데이트
    - 클립보드 작업을 위한 재시도 메커니즘 및 대체 방법 추가

--- CODE_Fixer/Form1.Designer.cs
+++ CODE_Fixer/Form1.Designer.cs
@@ -41,73 +41,75 @@
             this.cssButton = new Button();
             this.html5Button = new Button();
             this.jsButton = new Button();
-            this.mql5Button = new Button();
+            this.mql4Button = new Button();
+            this.mqhButton = new Button();
+            this.mq5Button = new Button();
             this.xmlButton = new Button();
             this.btnCopyToClipboard = new Button();
             this.btnClear = new Button();
-
+            this.alwaysOnTopCheckBox = new CheckBox();
             this.languageButtonsPanel.SuspendLayout();
             this.SuspendLayout();
-            //
+            // 
             // originalCodeLabel
-            //
+            // 
             this.originalCodeLabel.AutoSize = true;
             this.originalCodeLabel.Dock = DockStyle.Top;
-            this.originalCodeLabel.Location = new System.Drawing.Point(10, 12);
+            this.originalCodeLabel.Location = new Point(10, 12);
             this.originalCodeLabel.Name = "originalCodeLabel";
             this.originalCodeLabel.Padding = new Padding(0, 0, 0, 4);
-            this.originalCodeLabel.Size = new System.Drawing.Size(62, 19);
+            this.originalCodeLabel.Size = new Size(62, 19);
             this.originalCodeLabel.TabIndex = 0;
             this.originalCodeLabel.Text = "기존 코드:";
-            //
+            // 
             // originalCodeText
-            //
+            // 
             this.originalCodeText.Dock = DockStyle.Top;
-            this.originalCodeText.Location = new System.Drawing.Point(10, 31);
+            this.originalCodeText.Location = new Point(10, 31);
             this.originalCodeText.Margin = new Padding(3, 4, 3, 4);
             this.originalCodeText.MaxLength = 0;
             this.originalCodeText.Multiline = true;
             this.originalCodeText.Name = "originalCodeText";
             this.originalCodeText.ScrollBars = ScrollBars.Vertical;
-            this.originalCodeText.Size = new System.Drawing.Size(564, 200);
+            this.originalCodeText.Size = new Size(564, 200);
             this.originalCodeText.TabIndex = 1;
-            //
+            // 
             // modifiedCodeLabel
-            //
+            // 
             this.modifiedCodeLabel.AutoSize = true;
             this.modifiedCodeLabel.Dock = DockStyle.Top;
-            this.modifiedCodeLabel.Location = new System.Drawing.Point(10, 231);
+            this.modifiedCodeLabel.Location = new Point(10, 231);
             this.modifiedCodeLabel.Name = "modifiedCodeLabel";
             this.modifiedCodeLabel.Padding = new Padding(0, 12, 0, 4);
-            this.modifiedCodeLabel.Size = new System.Drawing.Size(62, 31);
+            this.modifiedCodeLabel.Size = new Size(62, 31);
             this.modifiedCodeLabel.TabIndex = 2;
             this.modifiedCodeLabel.Text = "수정 코드:";
-            //
+            // 
             // modifiedCodeText
-            //
+            // 
             this.modifiedCodeText.Dock = DockStyle.Top;
-            this.modifiedCodeText.Location = new System.Drawing.Point(10, 262);
+            this.modifiedCodeText.Location = new Point(10, 262);
             this.modifiedCodeText.Margin = new Padding(3, 4, 3, 4);
             this.modifiedCodeText.MaxLength = 0;
             this.modifiedCodeText.Multiline = true;
             this.modifiedCodeText.Name = "modifiedCodeText";
             this.modifiedCodeText.ScrollBars = ScrollBars.Vertical;
-            this.modifiedCodeText.Size = new System.Drawing.Size(564, 200);
+            this.modifiedCodeText.Size = new Size(564, 200);
             this.modifiedCodeText.TabIndex = 3;
-            //
+            // 
             // promptLabel
-            //
+            // 
             this.promptLabel.AutoSize = true;
             this.promptLabel.Dock = DockStyle.Top;
-            this.promptLabel.Location = new System.Drawing.Point(10, 462);
+            this.promptLabel.Location = new Point(10, 462);
             this.promptLabel.Name = "promptLabel";
             this.promptLabel.Padding = new Padding(0, 12, 0, 4);
-            this.promptLabel.Size = new System.Drawing.Size(238, 31);
+            this.promptLabel.Size = new Size(238, 31);
             this.promptLabel.TabIndex = 4;
             this.promptLabel.Text = "프롬프트 (클립보드 최상단에 포함될 내용):";
-            //
+            // 
             // languageButtonsPanel
-            //
+            // 
             this.languageButtonsPanel.AutoSize = true;
             this.languageButtonsPanel.Controls.Add(this.csharpButton);
             this.languageButtonsPanel.Controls.Add(this.xmlButton);
@@ -115,126 +117,162 @@
             this.languageButtonsPanel.Controls.Add(this.cssButton);
             this.languageButtonsPanel.Controls.Add(this.html5Button);
             this.languageButtonsPanel.Controls.Add(this.jsButton);
-            this.languageButtonsPanel.Controls.Add(this.mql5Button);
+            this.languageButtonsPanel.Controls.Add(this.mql4Button);
+            this.languageButtonsPanel.Controls.Add(this.mqhButton);
+            this.languageButtonsPanel.Controls.Add(this.mq5Button);
             this.languageButtonsPanel.Dock = DockStyle.Top;
-            this.languageButtonsPanel.Location = new System.Drawing.Point(10, 493);
+            this.languageButtonsPanel.Location = new Point(10, 493);
             this.languageButtonsPanel.Name = "languageButtonsPanel";
             this.languageButtonsPanel.Padding = new Padding(0, 0, 0, 5);
-            this.languageButtonsPanel.Size = new System.Drawing.Size(564, 41);
+            this.languageButtonsPanel.Size = new Size(564, 77);
             this.languageButtonsPanel.TabIndex = 8;
-            //
+            // 
             // csharpButton
-            //
-            this.csharpButton.Location = new System.Drawing.Point(3, 3);
+            // 
+            this.csharpButton.Location = new Point(3, 3);
             this.csharpButton.Name = "csharpButton";
-            this.csharpButton.Size = new System.Drawing.Size(75, 30);
+            this.csharpButton.Size = new Size(75, 30);
             this.csharpButton.TabIndex = 0;
             this.csharpButton.Tag = "C#";
             this.csharpButton.Text = "C#";
             this.csharpButton.UseVisualStyleBackColor = true;
             this.csharpButton.Click += this.languageButton_Click;
-            //
+            // 
             // xmlButton
-            //
-            this.xmlButton.Location = new System.Drawing.Point(84, 3);
+            // 
+            this.xmlButton.Location = new Point(84, 3);
             this.xmlButton.Name = "xmlButton";
-            this.xmlButton.Size = new System.Drawing.Size(75, 30);
+            this.xmlButton.Size = new Size(75, 30);
             this.xmlButton.TabIndex = 6;
             this.xmlButton.Tag = "XML";
             this.xmlButton.Text = "XML";
             this.xmlButton.UseVisualStyleBackColor = true;
             this.xmlButton.Click += this.languageButton_Click;
-            //
+            // 
             // pythonButton
-            //
-            this.pythonButton.Location = new System.Drawing.Point(165, 3);
+            // 
+            this.pythonButton.Location = new Point(165, 3);
             this.pythonButton.Name = "pythonButton";
-            this.pythonButton.Size = new System.Drawing.Size(75, 30);
+            this.pythonButton.Size = new Size(75, 30);
             this.pythonButton.TabIndex = 1;
             this.pythonButton.Tag = "Python";
             this.pythonButton.Text = "Python";
             this.pythonButton.UseVisualStyleBackColor = true;
             this.pythonButton.Click += this.languageButton_Click;
-            //
+            // 
             // cssButton
-            //
-            this.cssButton.Location = new System.Drawing.Point(246, 3);
+            // 
+            this.cssButton.Location = new Point(246, 3);
             this.cssButton.Name = "cssButton";
-            this.cssButton.Size = new System.Drawing.Size(75, 30);
+            this.cssButton.Size = new Size(75, 30);
             this.cssButton.TabIndex = 2;
             this.cssButton.Tag = "CSS";
             this.cssButton.Text = "CSS";
             this.cssButton.UseVisualStyleBackColor = true;
             this.cssButton.Click += this.languageButton_Click;
-            //
+            // 
             // html5Button
-            //
-            this.html5Button.Location = new System.Drawing.Point(327, 3);
+            // 
+            this.html5Button.Location = new Point(327, 3);
             this.html5Button.Name = "html5Button";
-            this.html5Button.Size = new System.Drawing.Size(75, 30);
+            this.html5Button.Size = new Size(75, 30);
             this.html5Button.TabIndex = 3;
             this.html5Button.Tag = "HTML5";
             this.html5Button.Text = "HTML5";
             this.html5Button.UseVisualStyleBackColor = true;
             this.html5Button.Click += this.languageButton_Click;
-            //
+            // 
             // jsButton
-            //
-            this.jsButton.Location = new System.Drawing.Point(408, 3);
+            // 
+            this.jsButton.Location = new Point(408, 3);
             this.jsButton.Name = "jsButton";
-            this.jsButton.Size = new System.Drawing.Size(75, 30);
+            this.jsButton.Size = new Size(75, 30);
             this.jsButton.TabIndex = 4;
             this.jsButton.Tag = "JavaScript";
             this.jsButton.Text = "JS";
             this.jsButton.UseVisualStyleBackColor = true;
             this.jsButton.Click += this.languageButton_Click;
-            //
-            // mql5Button
-            //
-            this.mql5Button.Location = new System.Drawing.Point(489, 3);
-            this.mql5Button.Name = "mql5Button";
-            this.mql5Button.Size = new System.Drawing.Size(75, 30);
-            this.mql5Button.TabIndex = 5;
-            this.mql5Button.Tag = "MQL5";
-            this.mql5Button.Text = "MQL5";
-            this.mql5Button.UseVisualStyleBackColor = true;
-            this.mql5Button.Click += this.languageButton_Click;
-            //
+            // 
+            // mql4Button
+            // 
+            this.mql4Button.Location = new Point(489, 3);
+            this.mql4Button.Name = "mql4Button";
+            this.mql4Button.Size = new Size(75, 30);
+            this.mql4Button.TabIndex = 5;
+            this.mql4Button.Tag = "MQL4";
+            this.mql4Button.Text = "MQL4";
+            this.mql4Button.UseVisualStyleBackColor = true;
+            this.mql4Button.Click += this.languageButton_Click;
+            // 
+            // mqhButton
+            // 
+            this.mqhButton.Location = new Point(3, 39);
+            this.mqhButton.Name = "mqhButton";
+            this.mqhButton.Size = new Size(75, 30);
+            this.mqhButton.TabIndex = 7;
+            this.mqhButton.Tag = "MQH";
+            this.mqhButton.Text = "MQH";
+            this.mqhButton.UseVisualStyleBackColor = true;
+            this.mqhButton.Click += this.languageButton_Click;
+            // 
+            // mq5Button
+            // 
+            this.mq5Button.Location = new Point(84, 39);
+            this.mq5Button.Name = "mq5Button";
+            this.mq5Button.Size = new Size(75, 30);
+            this.mq5Button.TabIndex = 8;
+            this.mq5Button.Tag = "MQ5";
+            this.mq5Button.Text = "MQ5";
+            this.mq5Button.UseVisualStyleBackColor = true;
+            this.mq5Button.Click += this.languageButton_Click;
+            // 
             // promptText
-            //
-            this.promptText.Location = new System.Drawing.Point(10, 534);
+            // 
+            this.promptText.Location = new Point(10, 570);
             this.promptText.Margin = new Padding(3, 4, 3, 4);
             this.promptText.Multiline = true;
             this.promptText.Name = "promptText";
             this.promptText.ScrollBars = ScrollBars.Vertical;
-            this.promptText.Size = new System.Drawing.Size(564, 120);
+            this.promptText.Size = new Size(564, 120);
             this.promptText.TabIndex = 5;
-            //
+            // 
             // btnCopyToClipboard
-            //
-            this.btnCopyToClipboard.Location = new System.Drawing.Point(12, 670);
+            // 
+            this.btnCopyToClipboard.Location = new Point(12, 706);
             this.btnCopyToClipboard.Name = "btnCopyToClipboard";
-            this.btnCopyToClipboard.Size = new System.Drawing.Size(280, 40);
+            this.btnCopyToClipboard.Size = new Size(245, 40);
             this.btnCopyToClipboard.TabIndex = 9;
             this.btnCopyToClipboard.Text = "클립보드에 복사";
             this.btnCopyToClipboard.UseVisualStyleBackColor = true;
-            this.btnCopyToClipboard.Click += new System.EventHandler(this.btnCopyToClipboard_Click);
-            //
+            this.btnCopyToClipboard.Click += this.btnCopyToClipboard_Click;
+            // 
             // btnClear
-            //
-            this.btnClear.Location = new System.Drawing.Point(298, 670);
+            // 
+            this.btnClear.Location = new Point(263, 706);
             this.btnClear.Name = "btnClear";
-            this.btnClear.Size = new System.Drawing.Size(280, 40);
+            this.btnClear.Size = new Size(160, 40);
             this.btnClear.TabIndex = 10;
             this.btnClear.Text = "지우기";
             this.btnClear.UseVisualStyleBackColor = true;
-            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
-            //
+            this.btnClear.Click += this.btnClear_Click;
+            // 
+            // alwaysOnTopCheckBox
+            // 
+            this.alwaysOnTopCheckBox.AutoSize = true;
+            this.alwaysOnTopCheckBox.Location = new Point(438, 716);
+            this.alwaysOnTopCheckBox.Name = "alwaysOnTopCheckBox";
+            this.alwaysOnTopCheckBox.Size = new Size(90, 19);
+            this.alwaysOnTopCheckBox.TabIndex = 11;
+            this.alwaysOnTopCheckBox.Text = "항상 위에";
+            this.alwaysOnTopCheckBox.UseVisualStyleBackColor = true;
+            this.alwaysOnTopCheckBox.CheckedChanged += this.alwaysOnTopCheckBox_CheckedChanged;
+            // 
             // Form1
-            //
-            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            // 
+            this.AutoScaleDimensions = new SizeF(7F, 15F);
             this.AutoScaleMode = AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(584, 730);
+            this.ClientSize = new Size(584, 766);
+            this.Controls.Add(this.alwaysOnTopCheckBox);
             this.Controls.Add(this.btnClear);
             this.Controls.Add(this.btnCopyToClipboard);
             this.Controls.Add(this.promptText);
@@ -244,9 +282,9 @@
             this.Controls.Add(this.modifiedCodeLabel);
             this.Controls.Add(this.originalCodeText);
             this.Controls.Add(this.originalCodeLabel);
-            this.Icon = (System.Drawing.Icon)resources.GetObject("$this.Icon");
+            this.Icon = (Icon)resources.GetObject("$this.Icon");
             this.Margin = new Padding(3, 4, 3, 4);
-            this.MinimumSize = new System.Drawing.Size(400, 615);
+            this.MinimumSize = new Size(600, 805);
             this.Name = "Form1";
             this.Padding = new Padding(10, 12, 10, 12);
             this.Text = "코드 형식 생성 및 복사 도구";
@@ -256,6 +294,7 @@
             this.languageButtonsPanel.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
+
         }
 
         #endregion
@@ -272,9 +311,12 @@
         private Button cssButton;
         private Button html5Button;
         private Button jsButton;
-        private Button mql5Button;
+        private Button mql4Button;
+        private Button mqhButton;
+        private Button mq5Button;
         private Button xmlButton;
         private Button btnCopyToClipboard;
         private Button btnClear;
+        private CheckBox alwaysOnTopCheckBox;
     }
 }
--- CODE_Fixer/Form1.cs
+++ CODE_Fixer/Form1.cs
@@ -5,12 +5,42 @@ using System.Text.Encodings.Web;
 using System.Text.Json;
 using System.Collections.Generic;
 using System.Windows.Forms;
+using System.Threading;
+using System.Runtime.InteropServices; // [수정코드] 추가
 
 namespace CODE_Fixer // 프로젝트 이름과 동일해야 합니다.
 {
     // partial 키워드는 이 클래스가 Form1.Designer.cs 파일과 합쳐진다는 것을 의미합니다.
     public partial class Form1 : Form
     {
+        // Windows API 함수들 추가 [수정코드]
+        [DllImport("user32.dll")]
+        static extern bool OpenClipboard(IntPtr hWndNewOwner);
+
+        [DllImport("user32.dll")]
+        static extern bool CloseClipboard();
+
+        [DllImport("user32.dll")]
+        static extern bool EmptyClipboard();
+
+        [DllImport("user32.dll")]
+        static extern IntPtr SetClipboardData(uint uFormat, IntPtr hMem);
+
+        [DllImport("kernel32.dll")]
+        static extern IntPtr GlobalAlloc(uint uFlags, UIntPtr dwBytes);
+
+        [DllImport("kernel32.dll")]
+        static extern IntPtr GlobalLock(IntPtr hMem);
+
+        [DllImport("kernel32.dll")]
+        static extern bool GlobalUnlock(IntPtr hMem);
+
+        [DllImport("kernel32.dll")]
+        static extern UIntPtr GlobalSize(IntPtr hMem);
+
+        private const uint CF_UNICODETEXT = 13;
+        private const uint GMEM_MOVEABLE = 0x0002;
+
         // 설정 파일 이름을 상수로 정의합니다.
         private const string SETTINGS_FILE = "settings.json";
 
@@ -24,9 +54,10 @@ namespace CODE_Fixer // 프로젝트 이름과 동일해야 합니다.
         }
 
         // 설정(JSON)을 저장하기 위한 데이터 구조(클래스)
-        public class Settings
+        public class AppSettings
         {
-            public string Prompt { get; set; } = string.Empty;
+            public Dictionary<string, string> Prompts { get; set; } = new Dictionary<string, string>();
+            public bool AlwaysOnTop { get; set; }
         }
 
         /// <summary>
@@ -39,16 +70,18 @@ namespace CODE_Fixer // 프로젝트 이름과 동일해야 합니다.
                 if (File.Exists(SETTINGS_FILE))
                 {
                     string json = File.ReadAllText(SETTINGS_FILE);
-                    var settings = JsonSerializer.Deserialize<Dictionary<string, string>>(json);
+                    var settings = JsonSerializer.Deserialize<AppSettings>(json);
 
                     if (settings != null)
                     {
-                        prompts = settings;
+                        prompts = settings.Prompts ?? new Dictionary<string, string>();
+                        alwaysOnTopCheckBox.Checked = settings.AlwaysOnTop;
+                        this.TopMost = settings.AlwaysOnTop; // 폼 로드 시 '항상 위에' 상태 적용
                     }
                 }
                 else
                 {
-                    // 기본 프롬프트 설정 (XML 추가)
+                    // 기본 프롬프트 설정 (MQH, MQ5 추가)
                     prompts = new Dictionary<string, string>
                     {
                         ["C#"] = "[기존코드]에 [수정코드]를 적용시켜서 생략없이 전체코드 출력해줘. 너가 코드를 해석해서 추가하거나 그러지 말고 문법적으로 잘 맞게 수정코드를 기존코드에 잘 적용시켜주면 된다. 이것은 C#이다. 그리고 출력 후 어디를 수정했는지도 알려줘, 코드블록으로 제공해줘",
@@ -57,7 +90,9 @@ namespace CODE_Fixer // 프로젝트 이름과 동일해야 합니다.
                         ["CSS"] = "[기존코드]에 [수정코드]를 적용시켜서 생략없이 전체코드 출력해줘. 너가 코드를 해석해서 추가하거나 그러지 말고 문법적으로 잘 맞게 수정코드를 기존코드에 잘 적용시켜주면 된다. 이것은 CSS이다. 그리고 출력 후 어디를 수정했는지도 알려줘, 코드블록으로 제공해줘",
                         ["HTML5"] = "[기존코드]에 [수정코드]를 적용시켜서 생략없이 전체코드 출력해줘. 너가 코드를 해석해서 추가하거나 그러지 말고 문법적으로 잘 맞게 수정코드를 기존코드에 잘 적용시켜주면 된다. 이것은 HTML5이다. 그리고 출력 후 어디를 수정했는지도 알려줘, 코드블록으로 제공해줘",
                         ["JavaScript"] = "[기존코드]에 [수정코드]를 적용시켜서 생략없이 전체코드 출력해줘. 너가 코드를 해석해서 추가하거나 그러지 말고 문법적으로 잘 맞게 수정코드를 기존코드에 잘 적용시켜주면 된다. 이것은 JavaScript이다. 그리고 출력 후 어디를 수정했는지도 알려줘, 코드블록으로 제공해줘",
-                        ["MQL5"] = "[기존코드]에 [수정코드]를 적용시켜서 생략없이 전체코드 출력해줘. 너가 코드를 해석해서 추가하거나 그러지 말고 문법적으로 잘 맞게 수정코드를 기존코드에 잘 적용시켜주면 된다. 이것은 MQL5이다. 그리고 출력 후 어디를 수정했는지도 알려줘, 코드블록으로 제공해줘"
+                        ["MQL4"] = "[기존코드]에 [수정코드]를 적용시켜서 생략없이 전체코드 출력해줘. 너가 코드를 해석해서 추가하거나 그러지 말고 문법적으로 잘 맞게 수정코드를 기존코드에 잘 적용시켜주면 된다. 이것은 MQL4이다. 그리고 출력 후 어디를 수정했는지도 알려줘, 코드블록으로 제공해줘",
+                        ["MQH"] = "[기존코드]에 [수정코드]를 적용시켜서 생략없이 전체코드 출력해줘. 너가 코드를 해석해서 추가하거나 그러지 말고 문법적으로 잘 맞게 수정코드를 기존코드에 잘 적용시켜주면 된다. 이것은 MQH이다. 그리고 출력 후 어디를 수정했는지도 알려줘, 코드블록으로 제공해줘",
+                        ["MQ5"] = "[기존코드]에 [수정코드]를 적용시켜서 생략없이 전체코드 출력해줘. 너가 코드를 해석해서 추가하거나 그러지 말고 문법적으로 잘 맞게 수정코드를 기존코드에 잘 적용시켜주면 된다. 이것은 MQ5이다. 그리고 출력 후 어디를 수정했는지도 알려줘, 코드블록으로 제공해줘"
                     };
                     SaveSettings();
                 }
@@ -75,12 +110,18 @@ namespace CODE_Fixer // 프로젝트 이름과 동일해야 합니다.
         {
             try
             {
+                var settings = new AppSettings
+                {
+                    Prompts = this.prompts,
+                    AlwaysOnTop = this.alwaysOnTopCheckBox.Checked
+                };
+
                 var options = new JsonSerializerOptions
                 {
                     WriteIndented = true,
                     Encoder = JavaScriptEncoder.UnsafeRelaxedJsonEscaping
                 };
-                string jsonString = JsonSerializer.Serialize(prompts, options);
+                string jsonString = JsonSerializer.Serialize(settings, options);
                 File.WriteAllText(SETTINGS_FILE, jsonString, Encoding.UTF8);
             }
             catch (Exception ex)
@@ -89,7 +130,103 @@ namespace CODE_Fixer // 프로젝트 이름과 동일해야 합니다.
             }
         }
 
-        // '클립보드에 복사' 버튼을 클릭했을 때 실행되는 메서드입니다.
+        /// <summary>
+        /// 강제로 클립보드를 해제하고 텍스트를 복사하는 메서드 [수정코드]
+        /// </summary>
+        /// <param name="text">복사할 텍스트</param>
+        /// <returns>복사 성공 여부</returns>
+        private bool ForceSetClipboard(string text)
+        {
+            IntPtr hGlobal = IntPtr.Zero;
+
+            try
+            {
+                // 1단계: 클립보드 강제 해제 및 열기
+                for (int attempt = 0; attempt < 5; attempt++)
+                {
+                    try
+                    {
+                        // 기존 클립보드 연결 강제 해제
+                        CloseClipboard();
+                        Thread.Sleep(50);
+
+                        // 클립보드 열기 시도
+                        if (OpenClipboard(this.Handle))
+                        {
+                            break;
+                        }
+
+                        if (attempt == 4)
+                        {
+                            return false; // 마지막 시도도 실패
+                        }
+
+                        Thread.Sleep(100 * (attempt + 1)); // 점진적 대기
+                    }
+                    catch
+                    {
+                        Thread.Sleep(100);
+                    }
+                }
+
+                // 2단계: 클립보드 비우기
+                if (!EmptyClipboard())
+                {
+                    CloseClipboard();
+                    return false;
+                }
+
+                // 3단계: 메모리 할당 및 텍스트 복사
+                byte[] bytes = Encoding.Unicode.GetBytes(text + "\0"); // null 종료 문자 추가
+                hGlobal = GlobalAlloc(GMEM_MOVEABLE, (UIntPtr)bytes.Length);
+
+                if (hGlobal == IntPtr.Zero)
+                {
+                    CloseClipboard();
+                    return false;
+                }
+
+                IntPtr pGlobal = GlobalLock(hGlobal);
+                if (pGlobal == IntPtr.Zero)
+                {
+                    CloseClipboard();
+                    return false;
+                }
+
+                Marshal.Copy(bytes, 0, pGlobal, bytes.Length);
+                GlobalUnlock(hGlobal);
+
+                // 4단계: 클립보드에 데이터 설정
+                IntPtr result = SetClipboardData(CF_UNICODETEXT, hGlobal);
+                if (result == IntPtr.Zero)
+                {
+                    CloseClipboard();
+                    return false;
+                }
+
+                // 성공 시 hGlobal은 시스템이 관리하므로 해제하지 않음
+                hGlobal = IntPtr.Zero;
+
+                return true;
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+            finally
+            {
+                // 클립보드 닫기
+                CloseClipboard();
+
+                // 실패 시에만 메모리 해제
+                if (hGlobal != IntPtr.Zero)
+                {
+                    Marshal.FreeHGlobal(hGlobal);
+                }
+            }
+        }
+
+        // '클립보드에 복사' 버튼을 클릭했을 때 실행되는 메서드입니다. [수정코드]
         private void btnCopyToClipboard_Click(object sender, EventArgs e)
         {
             string originalCode = originalCodeText.Text;
@@ -108,6 +245,9 @@ namespace CODE_Fixer // 프로젝트 이름과 동일해야 합니다.
                 return;
             }
 
+            btnCopyToClipboard.Enabled = false;
+            btnCopyToClipboard.Text = "강제 복사 중...";
+
             try
             {
                 string outputText = $@"{prompt}
@@ -117,13 +257,39 @@ namespace CODE_Fixer // 프로젝트 이름과 동일해야 합니다.
 [수정코드]
 {modifiedCode}";
 
-                Clipboard.SetText(outputText);
-                MessageBox.Show("요청하신 내용이 클립보드에 복사되었습니다.", "알림", MessageBoxButtons.OK, MessageBoxIcon.Information);
-                SaveSettings(); // 성공 시 프롬프트 저장
+                // 강제 클립보드 복사 시도
+                bool success = ForceSetClipboard(outputText);
+
+                if (success)
+                {
+                    MessageBox.Show("클립보드 강제 복사가 완료되었습니다!", "성공", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    SaveSettings();
+                }
+                else
+                {
+                    // 최후의 수단: .NET 방식으로 재시도
+                    try
+                    {
+                        Clipboard.Clear();
+                        Thread.Sleep(200);
+                        Clipboard.SetText(outputText);
+                        MessageBox.Show("클립보드 복사가 완료되었습니다.", "성공", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                        SaveSettings();
+                    }
+                    catch
+                    {
+                        MessageBox.Show("클립보드 복사에 실패했습니다.\n시스템을 재시작하거나 다른 프로그램을 종료 후 다시 시도해주세요.", "실패", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    }
+                }
             }
             catch (Exception ex)
             {
-                MessageBox.Show($"처리 중 오류 발생: {ex.Message}", "오류", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                MessageBox.Show($"오류 발생: {ex.Message}", "오류", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            finally
+            {
+                btnCopyToClipboard.Enabled = true;
+                btnCopyToClipboard.Text = "클립보드에 복사";
             }
         }
 
@@ -171,9 +337,9 @@ namespace CODE_Fixer // 프로젝트 이름과 동일해야 합니다.
         private void btnXML_Click(object sender, EventArgs e)
         {
             string xmlPrompt = "[기존코드]에 [수정코드]를 적용시켜서 생략없이 전체코드 출력해줘. " +
-                              "너가 코드를 해석해서 추가하거나 그러지 말고 문법적으로 잘 맞게 수정코드를 " +
-                              "기존코드에 잘 적용시켜주면 된다. 이것은 XML이다. " +
-                              "그리고 출력 후 어디를 수정했는지도 알려줘, 코드블록으로 제공해줘";
+                                 "너가 코드를 해석해서 추가하거나 그러지 말고 문법적으로 잘 맞게 수정코드를 " +
+                                 "기존코드에 잘 적용시켜주면 된다. 이것은 XML이다. " +
+                                 "그리고 출력 후 어디를 수정했는지도 알려줘, 코드블록으로 제공해줘";
 
             UpdatePromptText(xmlPrompt);
         }
@@ -182,6 +348,14 @@ namespace CODE_Fixer // 프로젝트 이름과 동일해야 합니다.
         private void UpdatePromptText(string text)
         {
             promptText.Text = text;
+        }
+
+        /// <summary>
+        /// '항상 위에' 체크박스 상태 변경 시 폼의 TopMost 속성을 설정합니다.
+        /// </summary>
+        private void alwaysOnTopCheckBox_CheckedChanged(object sender, EventArgs e)
+        {
+            this.TopMost = alwaysOnTopCheckBox.Checked;
         }
     }
 }
